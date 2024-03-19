4f4d6b17b5dad19583b87c9317f563fd91e1148a
CUNGVANTHANG <cungvanthang2k3@gmail.com>
2024-03-19 19:58:22 +0700

    Add word to dictionary

--- englishapp/src/main/java/org/base/DictionaryManagement.java
+++ englishapp/src/main/java/org/base/DictionaryManagement.java
@@ -1,17 +1,11 @@
 package org.base;
 
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
 public class DictionaryManagement extends Dictionary {
     // File Path
-    public static final String INPUT_PATH = "/src/main/resources/databases/test.txt";
-    public static final String OUTPUT_PATH = "/src/main/resources/databases/test_update.txt";
+    public static final String FILE_PATH = "/src/main/resources/databases/test.txt";
 
     // Handle Function
     public static String getAbsolutePath(String filePath) {
@@ -22,11 +16,34 @@ public class DictionaryManagement extends Dictionary {
         return currentDirectory + filePath;
     }
 
-    // File .txt Import
+    public static void handleSortWords() {
+        Collections.sort(dictionary, Comparator.comparing(Word::getWord_target));
+    }
+
+    // Method
+    public static void dictionaryExportToFile() {
+        try {
+            FileWriter fileWriter = new FileWriter(getAbsolutePath(FILE_PATH));
+            BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+
+            for (Word word : dictionary) {
+                bufferedWriter.write("@" + word.getWord_target() + " " + word.getWord_explain() + "\n");
+            }
+
+            bufferedWriter.flush();
+            bufferedWriter.close();
+            fileWriter.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
     public static void insertFromFile() {
         try {
             // File Reader
-            FileReader fileReader = new FileReader(getAbsolutePath(INPUT_PATH));
+            FileReader fileReader = new FileReader(getAbsolutePath(FILE_PATH));
             BufferedReader bufferedReader = new BufferedReader(fileReader);
 
             String line;
@@ -44,7 +61,7 @@ public class DictionaryManagement extends Dictionary {
                 }
                 if (line.startsWith("@")) {
                      if (line.indexOf("/") != -1) {
-                         word_target += line.substring(1, line.indexOf(" /")) + "\n";
+                         word_target += line.substring(1, line.indexOf(" /"));
                          word_explain += line.substring(line.indexOf("/")) + "\n";
                      } else {
                          word_target += line;
@@ -66,7 +83,6 @@ public class DictionaryManagement extends Dictionary {
         }
     }
 
-    // Commandline Import
     public static void insertFromCommandline() {
         Scanner input = new Scanner(System.in);
 
@@ -87,18 +103,23 @@ public class DictionaryManagement extends Dictionary {
         input.close();
     }
 
-    // Dictionary Export to File
-    public static void dictionaryExportToFile() {
-
-    }
-
-    // Lookup Dictionary
-    public static void dictionaryLookup() {
-
-    }
-
-    // Add Words to Dictionary
     public static void addWords(String word_target, String word_explain) {
+        // Convert to Lower case
+        word_target = word_target.toLowerCase();
+
+        // Get Index word_target
+        int index = Collections.binarySearch(dictionary,
+                new Word(word_target, null), Comparator.comparing(Word::getWord_target));
+
+        // Check Exist
+        if (index >= 0) {
+            System.out.println("Word exists");
+            return;
+        } else {
+
+        }
+
+        // Update Word to File
 
     }
 
@@ -107,14 +128,30 @@ public class DictionaryManagement extends Dictionary {
     }
 
     public static void removeWord(String word_target) {
-        // Convert to lower case
+        // Convert to Lower case
         word_target = word_target.toLowerCase();
 
-        // Get index word_target
+        // Get Index word_target
+        int index = Collections.binarySearch(dictionary,
+                new Word(word_target, null), Comparator.comparing(Word::getWord_target));
 
+        // Check Exist
+        if (index >= 0) {
+            dictionary.remove(dictionary.get(index));
+        } else {
+            System.out.println("Does not exist");
+        }
+
+        // Update Word to File
+        dictionaryExportToFile();
     }
 
-    public static void main(String[] args) {
+    // Lookup Dictionary
+    public static void dictionaryLookup() {
+        Scanner input = new Scanner(System.in);
 
+        input.close();
     }
+
+
 }
